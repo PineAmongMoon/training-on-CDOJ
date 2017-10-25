39ee5a918b0de431ac4d6e9f5ed2b40eb4faeded
俱源懋 / Ju Yuanmao <juyuanmao@icloud.com>
2017-10-25 17:28:10 +0800

    Update 528.cpp

--- 528.cpp
+++ 528.cpp
@@ -6,34 +6,6 @@
 //  Copyright © 2017年 Ju Yuanmao. All rights reserved.
 //
 
-/*#include <iostream>
-#include <map>
-using namespace std;
-int main() {
-    map<long, long, greater<long>> mp;
-    map<long, long, greater<long>>::iterator it;
-    long n, m, a, i;
-    ios::sync_with_stdio(false);
-    while (cin >> n >> m) {
-        mp.clear();
-        for (i=0; i<n; i++) {
-            cin >> a;
-            mp[a]++;
-        }
-        it = mp.begin();
-        while (m > 0) {
-            n = min(it->second, m);
-            for (i=0; i < n; i++) {
-                cout << it->first << ' ';
-            }
-            m -= n;
-            it++;
-        }
-        cout << endl;
-    }
-    return 0;
-}*/
-
 #include <iostream>
 #include <algorithm>
 #include <vector>
